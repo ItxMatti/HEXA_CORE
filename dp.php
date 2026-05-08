fc7e77396b2b566e458c55ddd61f2712c9325463
Matti Rehman <mattirehman343@gmail.com>
2026-05-09 01:59:54 +0500

    fix all conflicts properly

--- dp.php
+++ dp.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $host = "localhost";
 $user = "root";
@@ -10,17 +9,4 @@ $conn = new mysqli("localhost","root","","login_system");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-=======
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dp = "login_system";
-
-$conn = new mysqli("localhost","root","","login_system");
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
->>>>>>> 0f5eeac82164bc5624ec33175787a4be76c51815
 ?>
--- signup.php
+++ signup.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include "dp.php";
 ini_set('display_errors', 1);
@@ -23,30 +22,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     $stmt->close();
 }
-=======
-<?php
-include "dp.php";
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $dob = $_POST['dob'];
-    $gender = $_POST['gender'];
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $stmt = $conn->prepare("INSERT INTO users (firstname, lastname, email, phone, dob, gender, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssssss", $firstname, $lastname, $email, $phone, $dob, $gender, $username, $password);
-    if($stmt->execute()){
-        echo "Signup successful";
-    } else {
-        echo "Error: " . $stmt->error;
-    }
-    $stmt->close();
-}
->>>>>>> 0f5eeac82164bc5624ec33175787a4be76c51815
 ?>
